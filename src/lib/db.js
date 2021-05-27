7cb2c47680e9188f9bb53b9f30dead04e1150cdf
Víctor Aarón Bárcenas Mondragón <aaronbarcenasm@gmail.com>
2021-05-26 19:48:23 -0500

    third commit

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 
-const DB_USER = 'aaron'
-const DB_PASSWORD = 'hfo045no'
-const DB_HOST = 'novena-gen.fllyd.mongodb.net'
-const DB_NAME = 'kodemia'
+const { 
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_NAME
+  } = process.env
 
 const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
 
